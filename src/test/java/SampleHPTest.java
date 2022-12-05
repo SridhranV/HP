354206e2022308968c9f1511dbcb088a48921337
SridhranV <sridharancool97@gmail.com>
2022-12-05 15:41:30 +0530

    HP Work Flow

---
+++ src/test/java/SampleHPTest.java
@@ -0,0 +1,16 @@
+
+public class SampleHPTest
+{
+	private void method1()
+	{
+		System.out.println("3");
+	}
+	private void method2()
+	{
+		System.out.println("3");
+	}
+	private void method3()
+	{
+		System.out.println("3");
+	}
+}
